5d1a5c10d971f1c9298e29daea1e807482d81cf9
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-02-23 18:58:30 +1000

    Add program info

--- Assembly-CSharp/Properties/AssemblyInfo.cs
+++ Assembly-CSharp/Properties/AssemblyInfo.cs
@@ -4,3 +4,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyTitle("Grobda Remix Optimized Program Library")]
+[assembly: AssemblyDescription("written by Qinlili - GlacierLab")]
+[assembly: AssemblyCompany("GlacierLab")]
+[assembly: AssemblyProduct("GrobdaRemixOptimize")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
